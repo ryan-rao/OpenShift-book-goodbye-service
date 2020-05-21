2ee107fc2cb9b260e4e3fd824ae614ad4764417c
ryan-rao <ryan-rao@qq.com>
2020-05-21 18:22:55 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 
 function handler(request, response){
-    response.end("{'message':'Goodbye and see you next time!'}");
+    response.end("{'message':'This is first Goodbye, and see you next time!'}");
 }
 
 http.createServer(handler).listen(8080, function(){
